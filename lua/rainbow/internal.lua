37f5f47eba0bc603caded11d577fdd751984f353
Chinmay Dalal <chinmay.dalal.22012001@gmail.com>
2020-11-03 18:22:20 +0530

    some more stuff

--- lua/rainbow/internal.lua
+++ lua/rainbow/internal.lua
@@ -1,18 +1,22 @@
 local api = vim.api
 local ts = vim.treesitter
 local queries = require "nvim-treesitter.query"
-local configs = require "nvim-treesitter.configs"
 local parsers = require'nvim-treesitter.parsers'
+local nsid = vim.api.nvim_create_namespace('rainbow')
 
 local M = {}
 function M.attach(bufnr, lang)
   local parser = parsers.get_parser(bufnr, lang)
-  local config = configs.get_module('rainbow')
   local query = queries.get_query(lang, "parens")
+  local tree = parser:parse()
   if not query then return end
+  for id, node in query:iter_captures(tree:root(),0,1,20) do
+    -- typically useful info about the node:
+    local type = node:type() -- type of the captured node
+    local row1, col1, row2, col2 = node:range() -- range of the capture
+    vim.api.nvim_buf_set_extmark(0, nsid, row1, col1, {0, row2, col2, 'rainbowcol'})
+  end
 
-  M.highlighters[bufnr] = ts.highlighter.new(parser, query)
-  
 end
 
 function M.detach(bufnr)
