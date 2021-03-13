2a50293d08b2dd3afed167c694d676efbd693286
Chinmay Dalal <chinmay.dalal.22012001@gmail.com>
2021-03-13 12:41:17 +0530

    WIP

--- lua/rainbow.lua
+++ lua/rainbow.lua
@@ -1,19 +1,17 @@
-local queries = require "nvim-treesitter.query"
-local configs = require "nvim-treesitter.configs"
+local queries = require("nvim-treesitter.query")
+local configs = require("nvim-treesitter.configs")
 
 local M = {}
 
--- TODO: In this function replace `module-template` with the actual name of your module.
 function M.init()
-  require "nvim-treesitter".define_modules {
-    rainbow = {
-      module_path = "rainbow.internal",
-      is_supported = function(lang)
-        -- TODO: you don't want your queries to be named `awesome-query`, do you ?
-        return queries.get_query(lang, 'highlights') ~= nil
-      end
-    }
-  }
+    require("nvim-treesitter").define_modules({
+        rainbow = {
+            module_path = "rainbow.internal",
+            is_supported = function(lang)
+                return queries.get_query(lang, "parens") ~= nil
+            end,
+        },
+    })
 end
 
 return M
