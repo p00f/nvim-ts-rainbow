5ef9c414cef1419b8576c62f1079b22315c72a04
Chinmay Dalal <dalal.chinmay.0101@gmail.com>
2021-05-12 17:53:06 +0530

    Change formatting

--- lua/rainbow.lua
+++ lua/rainbow.lua
@@ -3,15 +3,15 @@ local queries = require("nvim-treesitter.query")
 local M = {}
 
 function M.init()
-        require("nvim-treesitter").define_modules({
-                rainbow = {
-                        module_path = "rainbow.internal",
-                        is_supported = function(lang)
-                                return queries.get_query(lang, "parens") ~= nil
-                        end,
-                        extended_mode = true,
-                },
-        })
+    require("nvim-treesitter").define_modules({
+        rainbow = {
+            module_path = "rainbow.internal",
+            is_supported = function(lang)
+                return queries.get_query(lang, "parens") ~= nil
+            end,
+            extended_mode = true,
+        },
+    })
 end
 
 return M
--- lua/rainbow/internal.lua
+++ lua/rainbow/internal.lua
@@ -8,13 +8,13 @@ local state_table = {} -- tracks which buffers have rainbow disabled
 
 -- Try to set colors from config
 local function set_colors(conf, name)
-        local config = conf.get_module("rainbow")
+    local config = conf.get_module("rainbow")
 
-        if config[name] ~= nil and type(config[name]) == "table" then
-                return config[name]
-        else
-                return require("rainbow." .. name)
-        end
+    if config[name] ~= nil and type(config[name]) == "table" then
+        return config[name]
+    else
+        return require("rainbow." .. name)
+    end
 end
 
 local colors = set_colors(configs, "colors")
@@ -22,118 +22,118 @@ local termcolors = set_colors(configs, "termcolors")
 
 -- define highlight groups
 for i = 1, #colors do
-        local s = "highlight default rainbowcol"
-                .. i
-                .. " guifg="
-                .. colors[i]
-                .. " ctermfg="
-                .. termcolors[i]
-        vim.cmd(s)
+    local s = "highlight default rainbowcol"
+        .. i
+        .. " guifg="
+        .. colors[i]
+        .. " ctermfg="
+        .. termcolors[i]
+    vim.cmd(s)
 end
 
 -- finds the nesting level of given node
 local function color_no(mynode, len)
-        local counter = 0
-        local current = mynode
-        while current:parent() ~= nil do
-                counter = counter + 1
-                current = current:parent()
-        end
-        if (counter % len == 0) then
-                return len
-        else
-                return (counter % len)
-        end
+    local counter = 0
+    local current = mynode
+    while current:parent() ~= nil do
+        counter = counter + 1
+        current = current:parent()
+    end
+    if counter % len == 0 then
+        return len
+    else
+        return (counter % len)
+    end
 end
 
 local callbackfn = function(bufnr, parser)
-        -- no need to do anything when pum is open
-        if vim.fn.pumvisible() == 1 then
-                return
+    -- no need to do anything when pum is open
+    if vim.fn.pumvisible() == 1 then
+        return
+    end
+
+    --clear highlights or code commented out later has highlights too
+    vim.api.nvim_buf_clear_namespace(bufnr, nsid, 0, -1)
+    parser:parse()
+    parser:for_each_tree(function(tree, lang_tree)
+        local root_node = tree:root()
+
+        local lang = lang_tree:lang()
+        local query = queries.get_query(lang, "parens")
+        if query ~= nil then
+            for _, node, _ in query:iter_captures(root_node, bufnr) do
+                -- set colour for this nesting level
+                local color_no_ = color_no(node, #colors)
+                local _, startCol, endRow, endCol = node:range() -- range of the capture, zero-indexed
+                vim.highlight.range(
+                    bufnr,
+                    nsid,
+                    ("rainbowcol" .. color_no_),
+                    { endRow, startCol },
+                    { endRow, endCol - 1 },
+                    "blockwise",
+                    true
+                )
+            end
         end
-
-        --clear highlights or code commented out later has highlights too
-        vim.api.nvim_buf_clear_namespace(bufnr, nsid, 0, -1)
-        parser:parse()
-        parser:for_each_tree(function(tree, lang_tree)
-                local root_node = tree:root()
-
-                local lang = lang_tree:lang()
-                local query = queries.get_query(lang, "parens")
-                if query ~= nil then
-                        for _, node, _ in query:iter_captures(root_node, bufnr) do
-                                -- set colour for this nesting level
-                                local color_no_ = color_no(node, #colors)
-                                local _, startCol, endRow, endCol = node:range() -- range of the capture, zero-indexed
-                                vim.highlight.range(
-                                        bufnr,
-                                        nsid,
-                                        ("rainbowcol" .. color_no_),
-                                        { endRow, startCol },
-                                        { endRow, endCol - 1 },
-                                        "blockwise",
-                                        true
-                                )
-                        end
-                end
-        end)
+    end)
 end
 
 local function try_async(f, bufnr, parser)
-        local cancel = false
-        return function()
-                if cancel then
-                        return true
-                end
-                local async_handle
-                async_handle = vim.loop.new_async(vim.schedule_wrap(function()
-                        f(bufnr, parser)
-                        async_handle:close()
-                end))
-                async_handle:send()
-        end, function()
-                cancel = true
+    local cancel = false
+    return function()
+        if cancel then
+            return true
         end
+        local async_handle
+        async_handle = vim.loop.new_async(vim.schedule_wrap(function()
+            f(bufnr, parser)
+            async_handle:close()
+        end))
+        async_handle:send()
+    end, function()
+        cancel = true
+    end
 end
 
 local function register_predicates(config)
-        for _, lang in ipairs(extended_languages) do
-                local enable_extended_mode
-                if type(config.extended_mode) == "table" then
-                        enable_extended_mode = config.extended_mode[lang]
-                else
-                        enable_extended_mode = config.extended_mode
-                end
-                nvim_query.add_predicate(lang .. "-extended-rainbow-mode?", function()
-                        return enable_extended_mode
-                end, true)
+    for _, lang in ipairs(extended_languages) do
+        local enable_extended_mode
+        if type(config.extended_mode) == "table" then
+            enable_extended_mode = config.extended_mode[lang]
+        else
+            enable_extended_mode = config.extended_mode
         end
+        nvim_query.add_predicate(lang .. "-extended-rainbow-mode?", function()
+            return enable_extended_mode
+        end, true)
+    end
 end
 
 local M = {}
 
 function M.attach(bufnr, lang)
-        local config = configs.get_module("rainbow")
-        local max_file_lines = config.max_file_lines
-        if max_file_lines ~= nil and vim.api.nvim_buf_line_count(bufnr) > max_file_lines then
-                return
-        end
+    local config = configs.get_module("rainbow")
+    local max_file_lines = config.max_file_lines
+    if max_file_lines ~= nil and vim.api.nvim_buf_line_count(bufnr) > max_file_lines then
+        return
+    end
 
-        local parser = parsers.get_parser(bufnr, lang)
-        register_predicates(config)
+    local parser = parsers.get_parser(bufnr, lang)
+    register_predicates(config)
 
-        local attachf, detachf = try_async(callbackfn, bufnr, parser)
-        state_table[bufnr] = detachf
-        callbackfn(bufnr, parser) -- do it on attach
-        vim.api.nvim_buf_attach(bufnr, false, { on_lines = attachf }) --do it on every change
+    local attachf, detachf = try_async(callbackfn, bufnr, parser)
+    state_table[bufnr] = detachf
+    callbackfn(bufnr, parser) -- do it on attach
+    vim.api.nvim_buf_attach(bufnr, false, { on_lines = attachf }) --do it on every change
 end
 
 function M.detach(bufnr)
-        local detachf = state_table[bufnr]
-        detachf()
-        local hlmap = vim.treesitter.highlighter.hl_map
-        hlmap["punctuation.bracket"] = "TSPunctBracket"
-        vim.api.nvim_buf_clear_namespace(bufnr, nsid, 0, -1)
+    local detachf = state_table[bufnr]
+    detachf()
+    local hlmap = vim.treesitter.highlighter.hl_map
+    hlmap["punctuation.bracket"] = "TSPunctBracket"
+    vim.api.nvim_buf_clear_namespace(bufnr, nsid, 0, -1)
 end
 
 return M
