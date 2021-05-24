e942ddf322eca525437119719c0fefed5a1394fb
Chinmay Dalal <dalal.chinmay.0101@gmail.com>
2021-05-24 07:12:50 +0530

    Test stylua workflow

--- lua/rainbow.lua
+++ lua/rainbow.lua
@@ -5,7 +5,7 @@ local M = {}
 function M.init()
     require("nvim-treesitter").define_modules({
         rainbow = {
-            module_path = "rainbow.internal",
+                      module_path = "rainbow.internal",
             is_supported = function(lang)
                 return queries.get_query(lang, "parens") ~= nil
             end,
