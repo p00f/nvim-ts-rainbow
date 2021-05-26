30af044b4fce74ebf56297fdb7b05855118bdcbe
Chinmay Dalal <dalal.chinmay.0101@gmail.com>
2021-05-26 18:21:03 +0530

    WIP

--- queries/cpp/parens.scm
+++ queries/cpp/parens.scm
@@ -5,17 +5,3 @@
 
 (template_parameter_list
   "<" @left ">" @right)
-
-[
-  (array_declarator)
-  (call_expression)
-  (compound_statement)
-  (enumerator_list)
-  (field_declaration_list)
-  (initializer_list)
-  (parameter_list)
-  (condition_clause)
-  (template_parameter_list)
-  (declaration_list) ] @rainbow.level
-
-(type_definition (template_type) @rainbow.level)
