184e7d5af9e6289eac8104dffd9b209b9ed9580f
Chinmay Dalal <dalal.chinmay.0101@gmail.com>
2022-01-28 17:40:28 +0530

    refactor, add emmylua

--- lua/rainbow/internal.lua
+++ lua/rainbow/internal.lua
@@ -7,10 +7,13 @@ local extended_languages = { "latex", "html" }
 local colors = configs.get_module("rainbow").colors
 local termcolors = configs.get_module("rainbow").termcolors
 
--- finds the nesting level of given node
-local function color_no(mynode, len, levels)
+--- Find the nesting level of a node.
+--- @param node table Node to find the level of
+--- @param len number Number of colours
+--- @param levels table Levels for the language
+local function color_no(node, len, levels)
     local counter = 0
-    local current = mynode
+    local current = node
     local found = false
     while current:parent() ~= nil do
         if levels then
@@ -33,13 +36,18 @@ local function color_no(mynode, len, levels)
     end
 end
 
-local function callbackfn(bufnr, changes, tree, lang)
+--- Update highlights for a range. Called every time text is changed.
+--- @param bufnr number Buffer number
+--- @param changes table Range of text changes
+--- @param tree table Syntax tree
+--- @param lang string Language
+local function update_range(bufnr, changes, tree, lang)
     if vim.fn.pumvisible() == 1 or not lang then
         return
     end
 
     for _, change in ipairs(changes) do
-        ----clear highlights or code commented out later has highlights too
+        --- clear highlights or code commented out later has highlights too
         vim.api.nvim_buf_clear_namespace(bufnr, nsid, change[1], change[3] + 1)
 
         local root_node = tree:root()
@@ -67,13 +75,17 @@ local function callbackfn(bufnr, changes, tree, lang)
     end
 end
 
+--- Update highlights for every tree in buffer.
+--- @param bufnr number Buffer number
 local function full_update(bufnr)
     local parser = parsers.get_parser(bufnr)
     parser:for_each_tree(function(tree, sub_parser)
-        callbackfn(bufnr, { { tree:root():range() } }, tree, sub_parser:lang())
+        update_range(bufnr, { { tree:root():range() } }, tree, sub_parser:lang())
     end)
 end
 
+--- Regsiter predicates for extended mode.
+--- @param config table Configuration for the `rainbow` module in nvim-treesitter
 local function register_predicates(config)
     local extended_mode
 
@@ -105,8 +117,8 @@ local state_table = {}
 
 local M = {}
 
+--- Define highlight groups. Needs to be a function to allow an autocmd doing this at colorscheme change.
 function M.defhl()
-    -- define highlight groups
     for i = 1, #colors do
         local s = "highlight default rainbowcol"
             .. i
@@ -120,6 +132,9 @@ end
 
 M.defhl()
 
+--- Attach module to buffer. Called when new buffer is opened or `:TSBufEnable rainbow`.
+--- @param bufnr number Buffer number
+--- @param lang string Buffer language
 function M.attach(bufnr, lang)
     local config = configs.get_module("rainbow")
     local max_file_lines = config.max_file_lines
@@ -133,7 +148,7 @@ function M.attach(bufnr, lang)
     parser:register_cbs({
         on_changedtree = function(changes, tree)
             if state_table[bufnr] == true then
-                callbackfn(bufnr, changes, tree, lang)
+                update_range(bufnr, changes, tree, lang)
             else
                 return
             end
@@ -141,6 +156,8 @@ function M.attach(bufnr, lang)
     })
 end
 
+--- Detach module from buffer. Called when `:TSBufDisable rainbow`.
+--- @param bufnr number Buffer number
 function M.detach(bufnr)
     state_table[bufnr] = false
     local hlmap = vim.treesitter.highlighter.hl_map
