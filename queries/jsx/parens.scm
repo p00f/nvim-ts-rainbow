415ebf42d3069807ad6ec55f220f92c3b73da918
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2022-08-06 13:17:54 +0200

    Enable highlight delimiters in JSX tags
    Highlight <, </, >, /> in JSX tags.
    
    This functionality was gone after changing treesitter queries to only
    highlight JSX tag names instead of entire tags.

--- queries/jsx/parens.scm
+++ queries/jsx/parens.scm
@@ -1,14 +1,19 @@
 ; inherits: square,round,curly
 
 ((jsx_opening_element
+   ["<" ">"] @left-delimiters
    name: _ @left
  ) (#jsx-extended-rainbow-mode?))
 
 ((jsx_self_closing_element
+   ; Combining "/" with "<" results in "invalid node type" treesitter error
+   ["<" "/" ">"] @self-closing-delimiters
    name: _ @self-closing
  ) (#jsx-extended-rainbow-mode?))
 
 ((jsx_closing_element
+   ; Combining "/" with ">" results in "invalid node type" treesitter error
+   ["<" "/" ">"] @right-delimiters
    name: _ @right
  ) (#jsx-extended-rainbow-mode?))
 
