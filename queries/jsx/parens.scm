a721fb160413866f7a2a167047064adb1a7010d2
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2022-08-07 18:44:32 +0200

    Fix TSX tag angle bracket highlights in extended mode

--- queries/jsx/parens.scm
+++ queries/jsx/parens.scm
@@ -1,21 +1,24 @@
 ; inherits: square,round,curly
 
 ((jsx_opening_element
-   ["<" ">"] @left-delimiters
+   "<" @left-tag-start
    name: _ @left
- ) (#jsx-extended-rainbow-mode?))
+   ">" @left-tag-end
+ ) (#jsx-extended-rainbow-mode? @left-tag-start @left-tag-end @left))
 
 ((jsx_self_closing_element
-   ; Combining "/" with "<" results in "invalid node type" treesitter error
-   ["<" "/" ">"] @self-closing-delimiters
-   name: _ @self-closing
- ) (#jsx-extended-rainbow-mode?))
+   "<"+ @self-closing-tag-start
+   name: _ @self-closing (#jsx-extended-rainbow-mode?)
+   ; Combining "/" with "<" results in no JSX highlights
+   ["/" ">"]+ @self-closing-tag-end
+ ) (#jsx-extended-rainbow-mode? @self-closing-tag-start @self-closing-tag-end @self-closing))
 
 ((jsx_closing_element
-   ; Combining "/" with ">" results in "invalid node type" treesitter error
-   ["<" "/" ">"] @right-delimiters
+   ; Combining "/" with "<" results in no JSX highlights
+   ["<" "/"]+ @right-tag-start
    name: _ @right
- ) (#jsx-extended-rainbow-mode?))
+   ">" @right-tag-end
+ ) (#jsx-extended-rainbow-mode? @right-tag-start @right-tag-end @right))
 
 ; [(jsx_element
 ;    open_tag: (jsx_opening_element) @left
