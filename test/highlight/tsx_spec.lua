2b1c0077fb2558702c167f655fb82a0c90419d66
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2022-08-07 17:37:28 +0200

    Detect extraneous and surrounding extmarks in the tests
    Refactor the tests to detect extraneous and surrounding extmarks.
    
    Extraneous extmarks are highlighted symbols that are not expected in the
    test source code DSL.
    
    Surrounding extmarks are extmarks that surround a given symbol that is
    expected to be highlighted. This is necessary because
    nvim_buf_get_extmarks returns only extmarks that are defined within a
    given range. If an extmark wraps a given range, it is not returned. This
    caused some TSX highlight tests to fail because `div` and other JSX
    elements were highlighted with a single extmark and asking for an
    extmark for the range of the `i` character (the middle one) returned 0
    extmarks, since the extmark that highlights that range surrounds the
    `i`.
    
    This required refactoring the tests to retrieve all buffer extmarks and
    check if all of them match the expected highlighted symbols and if there
    are no more leftover extmarks after matching them up with the
    expected ones.

--- test/highlight/tsx_spec.lua
+++ test/highlight/tsx_spec.lua
@@ -63,12 +63,95 @@ local function print_line_with_error_marker(line, line_number, error_marker_colu
     )
 end
 
+---@class HighlightedSymbol
+---@field line number 0-based
+---@field column number 0-based
+---@field hl_group string
+
+---@param extmarks table[] Extmark returned from nvim_buf_get_extmarks
+---@return HighlightedSymbol[]
+local function get_highlighted_symbols_from_extmarks(extmarks)
+    ---@type HighlightedSymbol[]
+    local highlighted_symbols = {}
+
+    for _, extmark in ipairs(extmarks) do
+        -- TODO: support multi-line extmarks
+        local line = extmark[2]
+        local start_col = extmark[3]
+        local details = extmark[4]
+        ---Extmark is end_col-exclusive
+        local end_col = details.end_col
+
+        for col = start_col, end_col - 1 do
+            table.insert(highlighted_symbols, {
+                line = line,
+                column = col,
+                hl_group = details.hl_group,
+            })
+        end
+    end
+
+    return highlighted_symbols
+end
+
+---Prunes duplicate highlighted symbols ensuring that each symbol is highlighted
+---with a single highlight group.
+---nvim-ts-rainbow sometimes sets duplicated extmarks to highlight symbols.
+---Not pruning duplicates would mean errors would be reported multiple times
+--(once for each duplicate extmark).
+---@param highlighted_symbols HighlightedSymbol[] The table of highlighted symbols. it will be modified in place.
+---@param source_code_lines string[] Source code lines used for error reporting
+local function remove_duplicate_highlighted_symbols(highlighted_symbols, source_code_lines)
+    local multiple_highlights_for_symbols = false
+
+    -- NOTE: manual index tracking because one loop iteration can remove
+    -- multiple elements. Using iterators could iterate over removed indices
+    local index = 1
+    while index <= #highlighted_symbols do
+        local symbol = highlighted_symbols[index]
+
+        -- The body of the loop tries to prune duplicates in the range of
+        -- index+1..#highlighted_symbols
+
+        -- NOTE: loop from the end to allow removing elements in the loop while
+        -- preserving indices that will be looped over in the future
+        for other_symbol_index = #highlighted_symbols, index + 1, -1 do
+            local other_symbol = highlighted_symbols[other_symbol_index]
+            if symbol.line == other_symbol.line and symbol.column == other_symbol.column then
+                if symbol.hl_group == other_symbol.hl_group then
+                    table.remove(highlighted_symbols, other_symbol_index)
+                else
+                    print("Symbol has multiple different highlight groups assigned to it.")
+                    print(
+                        string.format(
+                            "Found highlight groups: %s %s",
+                            symbol.hl_group,
+                            other_symbol.hl_group
+                        )
+                    )
+
+                    print_line_with_error_marker(
+                        source_code_lines[symbol.line + 1],
+                        symbol.line + 1,
+                        symbol.column
+                    )
+                    multiple_highlights_for_symbols = true
+                end
+            end
+        end
+
+        index = index + 1
+    end
+
+    assert(not multiple_highlights_for_symbols, "There are multiple highlights for some symbols")
+end
+
 describe("TSX highlighting", function()
     it("should highlight", function()
         -- TODO: enable based on file name
         require("nvim-treesitter.configs").get_module("rainbow").extended_mode = true
         vim.cmd.edit("test.tsx")
-        assert.equals("typescriptreact", vim.bo.filetype)
+        assert.equals("typescriptreact", vim.bo.filetype, "Invalid filetype")
 
         -- TODO: read source code from files
         -- https://github.com/nvim-treesitter/nvim-treesitter/blob/a9a6493b1eeba458757903352e0d3dc4b54fd4f2/tests/query/highlights_spec.lua#L100
@@ -78,7 +161,7 @@ describe("TSX highlighting", function()
     function MyComponent() {
 //hl                    11 1
       return <div>Hello world</div>;
-//hl         22222           222221
+//hl         22222           222222
     }
 //hl1
         ]]
@@ -87,28 +170,63 @@ describe("TSX highlighting", function()
         vim.api.nvim_buf_set_lines(0, 0, -1, true, source_code_lines)
 
         local rainbow_ns_id = vim.api.nvim_get_namespaces().rainbow_ns
-        assert.not_equal(nil, rainbow_ns_id)
+        assert.not_equal(nil, rainbow_ns_id, "rainbow namespace not found")
 
         local parser = require("nvim-treesitter.parsers").get_parser(0)
-        assert.truthy(parser)
+        assert.truthy(parser, "Parser not found")
         parser:parse()
 
+        -- NOTE: nvim_buf_get_extmarks does not return extmarks that contain
+        -- some range. It only returns extmarks within the given range.
+        -- We cannot use it to look for extmarks for a given symbol, because sometimes
+        -- the extmarks are for a range (e.g. when highlighting a JSX tag, the
+        -- whole range "div" is a single extmark and asking for an extmark for
+        -- the position of "i" returns nothing).
+        -- Thus, we must filter through all extmarks set on the buffer and
+        -- check each symbol.
+        local extmarks = vim.api.nvim_buf_get_extmarks(0, rainbow_ns_id, 0, -1, { details = true })
+        local highlighted_symbols = get_highlighted_symbols_from_extmarks(extmarks)
+
+        remove_duplicate_highlighted_symbols(highlighted_symbols, source_code_lines)
+
         for _, expected_highlight in ipairs(get_expected_highlights(source_code_lines)) do
-            local highlight_position = { expected_highlight.line, expected_highlight.column }
+            local symbol_highlighted = false
 
-            -- NOTE: does not return extmarks over a region when asking for a character inside of that region
-            -- TODO: refactor to get all buffer extmarks and then filter through it (`is_inside_extmark`)
-            local extmarks = vim.api.nvim_buf_get_extmarks(
-                0,
-                rainbow_ns_id,
-                highlight_position,
-                highlight_position,
-                { details = true }
-            )
+            -- NOTE: loop from the end to allow removing elements inside of the loop
+            -- without changing the indices that will be looped over
+            for i = #highlighted_symbols, 1, -1 do
+                local highlighted_symbol = highlighted_symbols[i]
+                if
+                    highlighted_symbol.line == expected_highlight.line
+                    and highlighted_symbol.column == expected_highlight.column
+                then
+                    symbol_highlighted = true
 
-            -- TODO: verify that there are no extraneous highlights
+                    local expected_highlight_group =
+                        get_rainbow_highlight_group_name(expected_highlight.rainbow_level)
+                    if expected_highlight_group ~= highlighted_symbol.hl_group then
+                        print(
+                            string.format(
+                                'Invalid rainbow highlight group. Expected "%s", found "%s"',
+                                expected_highlight_group,
+                                highlighted_symbol.hl_group
+                            )
+                        )
+                        print_line_with_error_marker(
+                            source_code_lines[expected_highlight.line + 1],
+                            expected_highlight.line + 1,
+                            expected_highlight.column
+                        )
+                    end
 
-            if vim.tbl_isempty(extmarks) then
+                    -- NOTE: remove the matched highlighted symbol to later
+                    -- check that all highlighted symbols matched some expected
+                    -- highlight
+                    table.remove(highlighted_symbols, i)
+                end
+            end
+
+            if not symbol_highlighted then
                 print(
                     string.format(
                         'No highlight groups detected. Expected "%s".',
@@ -121,28 +239,21 @@ describe("TSX highlighting", function()
                     expected_highlight.column
                 )
             end
-
-            -- NOTE: nvim-ts-rainbow sometimes sets duplicated extmarks to highlight.
-            -- The extmarks should be the same.
-            for _, extmark in ipairs(extmarks) do
-                local details = extmark[4]
-                local expected_highlight_group =
-                    get_rainbow_highlight_group_name(expected_highlight.rainbow_level)
-                if expected_highlight_group ~= details.hl_group then
-                    print(
-                        string.format(
-                            'Invalid rainbow highlight group. Expected "%s", found "%s"',
-                            expected_highlight_group,
-                            details.hl_group
-                        )
-                    )
-                    print_line_with_error_marker(
-                        source_code_lines[expected_highlight.line + 1],
-                        expected_highlight.line + 1,
-                        expected_highlight.column
-                    )
-                end
-            end
         end
+
+        for _, symbol in ipairs(highlighted_symbols) do
+            print(
+                string.format(
+                    'Symbol was extraneously highlighted with highlight group "%s"',
+                    symbol.hl_group
+                )
+            )
+            print_line_with_error_marker(
+                source_code_lines[symbol.line + 1],
+                symbol.line + 1,
+                symbol.column
+            )
+        end
+        assert(#highlighted_symbols == 0, "Extraneous highlights")
     end)
 end)
