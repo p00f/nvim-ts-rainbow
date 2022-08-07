5f8c292ccd9da55e362c0d0eaccba67b0cd846ff
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2022-08-07 18:43:23 +0200

    Enable file-based highlights testing
    Refactor the TSX tests to use files as test cases instead of
    hand-written tests.
    
    The test cases source code uses the DSL for defining expected highlight
    groups.

--- test/highlight/highlight_spec.lua
+++ test/highlight/highlight_spec.lua
@@ -146,114 +146,119 @@ local function remove_duplicate_highlighted_symbols(highlighted_symbols, source_
     assert(not multiple_highlights_for_symbols, "There are multiple highlights for some symbols")
 end
 
-describe("TSX highlighting", function()
-    it("should highlight", function()
-        -- TODO: enable based on file name
-        require("nvim-treesitter.configs").get_module("rainbow").extended_mode = true
-        vim.cmd.edit("test.tsx")
-        assert.equals("typescriptreact", vim.bo.filetype, "Invalid filetype")
+local function verify_highlights_in_file(filename)
+    local extended_mode = string.find(filename, "extended") ~= nil
+    local rainbow_module = require("nvim-treesitter.configs").get_module("rainbow")
+    rainbow_module.extended_mode = extended_mode
 
-        -- TODO: read source code from files
-        -- https://github.com/nvim-treesitter/nvim-treesitter/blob/a9a6493b1eeba458757903352e0d3dc4b54fd4f2/tests/query/highlights_spec.lua#L100
-        local source_code = [[
-    const abc = {};
-//hl            11
-    function MyComponent() {
-//hl                    11 1
-      return <div>Hello world</div>;
-//hl         22222           222222
-    }
-//hl1
-        ]]
-        local source_code_lines = vim.fn.split(source_code, "\n")
+    vim.cmd.edit(filename)
 
-        vim.api.nvim_buf_set_lines(0, 0, -1, true, source_code_lines)
+    local source_code_lines = vim.api.nvim_buf_get_lines(0, 0, -1, 1)
 
-        local rainbow_ns_id = vim.api.nvim_get_namespaces().rainbow_ns
-        assert.not_equal(nil, rainbow_ns_id, "rainbow namespace not found")
+    vim.api.nvim_buf_set_lines(0, 0, -1, true, source_code_lines)
 
-        local parser = require("nvim-treesitter.parsers").get_parser(0)
-        assert.truthy(parser, "Parser not found")
-        parser:parse()
+    local rainbow_ns_id = vim.api.nvim_get_namespaces().rainbow_ns
+    assert.not_equal(nil, rainbow_ns_id, "rainbow namespace not found")
 
-        -- NOTE: nvim_buf_get_extmarks does not return extmarks that contain
-        -- some range. It only returns extmarks within the given range.
-        -- We cannot use it to look for extmarks for a given symbol, because sometimes
-        -- the extmarks are for a range (e.g. when highlighting a JSX tag, the
-        -- whole range "div" is a single extmark and asking for an extmark for
-        -- the position of "i" returns nothing).
-        -- Thus, we must filter through all extmarks set on the buffer and
-        -- check each symbol.
-        local extmarks = vim.api.nvim_buf_get_extmarks(0, rainbow_ns_id, 0, -1, { details = true })
-        local highlighted_symbols = get_highlighted_symbols_from_extmarks(extmarks)
+    local parser = require("nvim-treesitter.parsers").get_parser(0)
+    assert.truthy(parser, "Parser not found")
+    parser:parse()
 
-        remove_duplicate_highlighted_symbols(highlighted_symbols, source_code_lines)
+    -- NOTE: nvim_buf_get_extmarks does not return extmarks that contain
+    -- some range. It only returns extmarks within the given range.
+    -- We cannot use it to look for extmarks for a given symbol, because sometimes
+    -- the extmarks are for a range (e.g. when highlighting a JSX tag, the
+    -- whole range "div" is a single extmark and asking for an extmark for
+    -- the position of "i" returns nothing).
+    -- Thus, we must filter through all extmarks set on the buffer and
+    -- check each symbol.
+    local extmarks = vim.api.nvim_buf_get_extmarks(0, rainbow_ns_id, 0, -1, { details = true })
+    local highlighted_symbols = get_highlighted_symbols_from_extmarks(extmarks)
 
-        for _, expected_highlight in ipairs(get_expected_highlights(source_code_lines)) do
-            local symbol_highlighted = false
+    remove_duplicate_highlighted_symbols(highlighted_symbols, source_code_lines)
 
-            -- NOTE: loop from the end to allow removing elements inside of the loop
-            -- without changing the indices that will be looped over
-            for i = #highlighted_symbols, 1, -1 do
-                local highlighted_symbol = highlighted_symbols[i]
-                if
-                    highlighted_symbol.line == expected_highlight.line
-                    and highlighted_symbol.column == expected_highlight.column
-                then
-                    symbol_highlighted = true
+    local some_symbol_not_highlighted = false
+    local invalid_highlight = false
+    for _, expected_highlight in ipairs(get_expected_highlights(source_code_lines)) do
+        local symbol_highlighted = false
 
-                    local expected_highlight_group =
-                        get_rainbow_highlight_group_name(expected_highlight.rainbow_level)
-                    if expected_highlight_group ~= highlighted_symbol.hl_group then
-                        print(
-                            string.format(
-                                'Invalid rainbow highlight group. Expected "%s", found "%s"',
-                                expected_highlight_group,
-                                highlighted_symbol.hl_group
-                            )
+        -- NOTE: loop from the end to allow removing elements inside of the loop
+        -- without changing the indices that will be looped over
+        for i = #highlighted_symbols, 1, -1 do
+            local highlighted_symbol = highlighted_symbols[i]
+            if
+                highlighted_symbol.line == expected_highlight.line
+                and highlighted_symbol.column == expected_highlight.column
+            then
+                symbol_highlighted = true
+
+                local expected_highlight_group =
+                    get_rainbow_highlight_group_name(expected_highlight.rainbow_level)
+                if expected_highlight_group ~= highlighted_symbol.hl_group then
+                    invalid_highlight = true
+                    print(
+                        string.format(
+                            'Invalid rainbow highlight group. Expected "%s", found "%s"',
+                            expected_highlight_group,
+                            highlighted_symbol.hl_group
                         )
-                        print_line_with_error_marker(
-                            source_code_lines[expected_highlight.line + 1],
-                            expected_highlight.line + 1,
-                            expected_highlight.column
-                        )
-                    end
-
-                    -- NOTE: remove the matched highlighted symbol to later
-                    -- check that all highlighted symbols matched some expected
-                    -- highlight
-                    table.remove(highlighted_symbols, i)
-                end
-            end
-
-            if not symbol_highlighted then
-                print(
-                    string.format(
-                        'No highlight groups detected. Expected "%s".',
-                        get_rainbow_highlight_group_name(expected_highlight.rainbow_level)
                     )
-                )
-                print_line_with_error_marker(
-                    source_code_lines[expected_highlight.line + 1],
-                    expected_highlight.line + 1,
-                    expected_highlight.column
-                )
+                    print_line_with_error_marker(
+                        source_code_lines[expected_highlight.line + 1],
+                        expected_highlight.line + 1,
+                        expected_highlight.column
+                    )
+                end
+
+                -- NOTE: remove the matched highlighted symbol to later
+                -- check that all highlighted symbols matched some expected
+                -- highlight
+                table.remove(highlighted_symbols, i)
             end
         end
 
-        for _, symbol in ipairs(highlighted_symbols) do
+        if not symbol_highlighted then
             print(
                 string.format(
-                    'Symbol was extraneously highlighted with highlight group "%s"',
-                    symbol.hl_group
+                    'No highlight groups detected. Expected "%s".',
+                    get_rainbow_highlight_group_name(expected_highlight.rainbow_level)
                 )
             )
             print_line_with_error_marker(
-                source_code_lines[symbol.line + 1],
-                symbol.line + 1,
-                symbol.column
+                source_code_lines[expected_highlight.line + 1],
+                expected_highlight.line + 1,
+                expected_highlight.column
             )
+            some_symbol_not_highlighted = true
         end
-        assert(#highlighted_symbols == 0, "Extraneous highlights")
-    end)
+    end
+
+    for _, symbol in ipairs(highlighted_symbols) do
+        print(
+            string.format(
+                'Symbol was extraneously highlighted with highlight group "%s"',
+                symbol.hl_group
+            )
+        )
+        print_line_with_error_marker(
+            source_code_lines[symbol.line + 1],
+            symbol.line + 1,
+            symbol.column
+        )
+    end
+    assert(not invalid_highlight, "Some symbol was incorrectly highlighted")
+    assert(not some_symbol_not_highlighted, "Some symbol was not highlighted")
+    assert(#highlighted_symbols == 0, "Extraneous highlights")
+end
+
+describe("Highlighting integration tests", function()
+    local files = vim.fn.glob("test/highlight/**/*.*", nil, true)
+
+    for _, filename in ipairs(files) do
+        if not string.match(filename, "highlight_spec.lua") then
+            it(filename, function()
+                verify_highlights_in_file(filename)
+            end)
+        end
+    end
 end)
