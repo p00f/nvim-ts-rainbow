642b11d708b4d82229ddacbd31ad72cb4f1c427e
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2022-08-07 19:26:49 +0200

    Add javascript and JSX highlight test cases
    The tests are failing right now because JSX elements are not
    highlighted.

---
+++ test/highlight/jsx/regular.jsx
@@ -0,0 +1,15 @@
+const abc = {};
+// hl       11
+// prettier-ignore
+function MyComponent() {
+    // hl           11 1
+    return (
+    // hl  2
+        <div>
+            <span>Hello world</span>
+            <img src="self closing" />
+        </div>
+     );
+// hl2
+     }
+// hl1
